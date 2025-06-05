f9ec455ae9e46c15bda5a17e585f04c1289351b9
elijahjoson <eajoson05@gmail.com>
2025-06-05 15:51:14 +0800

    Sponsor pets interface 2

---
+++ pawfile-sponsor-pets2.js
@@ -0,0 +1,9 @@
+// In the future, populate data dynamically if needed
+document.addEventListener("DOMContentLoaded", () => {
+    const params = new URLSearchParams(window.location.search);
+    const petId = params.get("petId");
+    if (petId) {
+      document.getElementById("petId").textContent = petId;
+    }
+  });
+  
